3538d1e537821a19487f858aaaa158a70b520177
Aaron <aaronmangbenza@gmail.com>
2022-12-02 02:28:22 +0100

    created the employee model class

--- src/main/java/org/hlab/ems/model/Employee.java
+++ src/main/java/org/hlab/ems/model/Employee.java
@@ -1,4 +1,75 @@
 package org.hlab.ems.model;
 
-public class Employee {
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Employee implements Serializable {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Employee(int id, String firstName, String lastName, int age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Employee)) return false;
+        Employee employee = (Employee) o;
+        return getId() == employee.getId() && getAge() == employee.getAge() && getFirstName().equals(employee.getFirstName()) && getLastName().equals(employee.getLastName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getFirstName(), getLastName(), getAge());
+    }
 }
